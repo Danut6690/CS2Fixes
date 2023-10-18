e5851550c228eb40c087725330e7c5438a7e478f
Moongetsu <moongetsu1@gmail.com>
2023-10-18 20:43:36 +0300

    Update ccsplayercontroller.h

--- src/cs2_sdk/entity/ccsplayercontroller.h
+++ src/cs2_sdk/entity/ccsplayercontroller.h
@@ -25,10 +25,12 @@
 class CCSPlayerController : public CBasePlayerController
 {
 public:
-	DECLARE_SCHEMA_CLASS(CCSPlayerController);
+    DECLARE_SCHEMA_CLASS(CCSPlayerController);
 
-	SCHEMA_FIELD(CCSPlayerController_InGameMoneyServices*, m_pInGameMoneyServices)
-	SCHEMA_FIELD(CCSPlayerController_ActionTrackingServices*, m_pActionTrackingServices)
+    SCHEMA_FIELD(CCSPlayerController_InGameMoneyServices*, m_pInGameMoneyServices)
+    SCHEMA_FIELD(CCSPlayerController_ActionTrackingServices*, m_pActionTrackingServices)
+    SCHEMA_FIELD(int, m_iScore)
+    SCHEMA_FIELD(int, m_iMVPs)
 
-	static CCSPlayerController* FromPawn(CCSPlayerPawn* pawn) { return (CCSPlayerController*)pawn->m_hController().Get(); }
+    static CCSPlayerController* FromPawn(CCSPlayerPawn* pawn) { return (CCSPlayerController*)pawn->m_hController().Get(); }
 };
