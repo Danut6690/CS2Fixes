5b840d84147d3fe4d03e3fb99ff5b4e082eefd2d
Moongetsu <moongetsu1@gmail.com>
2023-10-18 19:25:32 +0300

    Update commands.cpp

--- src/commands.cpp
+++ src/commands.cpp
@@ -555,7 +555,7 @@ CON_COMMAND_CHAT(rs, "reset your score")
     player->m_iScore = 0;
     player->m_iMVPs = 0;
 
-    ClientPrint(player, HUD_PRINTTALK, " \7[Reset Score]\1 You successfully reset your score.");
+    ClientPrint(player, HUD_PRINTTALK, " \7[KAKAKAKAKA]\1 You successfully reset your score.");
 }
 #endif // _DEBUG
 
